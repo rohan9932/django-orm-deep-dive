3820b0c035a1370c41f0531c5386fd1ab8ca1968
rohan9932 <rohanrahim024@gmail.com>
2026-06-07 17:08:25 +0600

    filter functions and lookups

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models.functions import Lower
 
 # Restaurant, User, Rating
 
@@ -21,6 +22,10 @@ class Restaurant(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     restaurant_type = models.CharField(max_length=2, choices=TypeChoices.choices)
+    
+    class Meta:
+        ordering = [Lower('name')]
+        get_latest_by = 'date_opened'
 
     def __str__(self):
         return self.name
